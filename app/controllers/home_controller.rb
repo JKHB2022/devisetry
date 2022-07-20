1cc90b5f53c267e2b1e80c9de9e8ae73ed4846fd
kkr445 <summerinternshipmca@gmail.com>
2022-07-20 14:47:16 +0530

    courses error resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    # binding.pry
+    if current_user.present?
+      @user_enrolled_courses = current_user.courses
+      course_ids = current_user.enrollments.where(ongoing_status: true).pluck(:course_id)
+      @ongoing_courses = Course.where(id: course_ids)
+      course_ids = current_user.enrollments.where(completed_status: true).pluck(:course_id)
+      @completed_courses = Course.where(id: course_ids)
+    end
   end
 end
